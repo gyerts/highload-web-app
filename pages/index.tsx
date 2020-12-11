457946ce6e9fab2f275aaab58d701a1fe71417ff
Yuriy Gyerts <gyerts@gmail.com>
2020-12-12 00:45:40 +0200

    init commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,21 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        {/*<title>{siteTitle}</title>*/}
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
@@ -38,7 +47,7 @@ export default function Home({ allPostsData }) {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
